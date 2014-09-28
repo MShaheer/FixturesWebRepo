e93e02bd77e5b385b876682b8533e85195942292
Shaheer <shaheernov123@gmail.com>
2014-09-28 13:28:44 +0500

    test action included commit2

--- Fixtures/Controllers/HomeController.cs
+++ Fixtures/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Fixtures.Controllers
             return View();
         }
 
+        public ActionResult TestAction()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
