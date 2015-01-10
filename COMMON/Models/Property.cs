22459f8dae55417b9f8f36e735cd28728cc56009
Shaheer <shaheernov123@gmail.com>
2015-01-10 15:34:39 +0500

    Commit By After Post Ad

--- COMMON/Models/Property.cs
+++ COMMON/Models/Property.cs
@@ -14,7 +14,7 @@ namespace COMMON.Models
         public string Location { get; set; }
         public int Accomodates { get; set; }
         public int Price { get; set; }
-        public string RateType { get; set; }
+        public string RoomType { get; set; }
         public string Description { get; set; }
         public int Rating { get; set; }
         public string Owner { get; set; }
