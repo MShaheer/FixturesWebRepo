226e1d2798424036f3e03da71bab5148cbc0fa6a
Shaheer <shaheernov123@gmail.com>
2015-01-13 00:07:26 +0500

    Including Common models folder to resolve merge conflict libgit 21 error

---
+++ COMMON/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMMON.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public List<Reference> References { get; set; }
+        public List<WishList> WishList { get; set; }
+    }
+}
